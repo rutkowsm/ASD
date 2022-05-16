c66fdd7909e53e48e51b3804d133dc3431683319
rutkowsm <37616390+rutkowsm@users.noreply.github.com>
2022-05-16 23:07:51 +0200

    Add files via upload

---
+++ Projekt1/InsertSort/insertsort_random.py
@@ -0,0 +1,44 @@
+import random
+
+from datetime import datetime
+
+try:
+    fname = "../basics/data/random.txt"
+
+    with open(fname) as f:
+
+        numbers = []
+        for line in f:
+            num = int(line)
+            numbers.append(num)
+
+    def insertSort(numbers):
+
+        for i in range(1, len(numbers)):
+            key = numbers[i]
+            j = 0
+
+            while key > numbers[j] and j < i:
+                j = j + 1
+
+            numbers.insert(j, key)
+            del numbers[i+1]
+
+        # print(numbers)
+
+    starttime = datetime.now()
+    print("InsertSort random. Elements: ", len(numbers))
+    print("Start time: ", starttime)
+
+    insertSort(numbers)
+
+    endtime = datetime.now()
+
+    print("End time: ", endtime)
+    timeTaken = endtime - starttime
+    print("Time taken: ", timeTaken)
+
+except FileNotFoundError as error:
+    print("File is not available:", type(error))
+except Exception as error:
+    print("Something went wrong:", type(error))
